c90e5cb82afd37cfcde57c8b08d1924ecf5b4624
johnhenry <john.henry@tutamail.com>
2026-07-20 15:34:01 +0100

    Updated code to match local database changes

--- art_project/settings.py
+++ art_project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'art_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'apache_red_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
